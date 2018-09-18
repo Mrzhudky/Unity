7d0bfcb5ac936ddd0884187ffe69b30c2ec11a56
Mrzhudky <1183260441@qq.com>
2018-09-18 22:17:15 +0800

    update

---
+++ UFO2DTutorial/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [Header("玩家物件")]
+    public GameObject player;
+
+    [Header("相对位移")]
+    public Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ UFO2DTutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    [Header("水平方")]
+    public float moveX;
+
+    [Header("垂直方")]
+    public float moveY;
+
+    [Header("推力")]
+    public float push = 6;
+
+    //刚体，以便施加推力
+    Rigidbody2D rb2D;
+
+    [Header("分数")]
+    public Text countText;
+
+    [Header("结束")]
+    public Text winText;
+
+    int score;
+
+	// Use this for initialization
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+
+        winText.text = "";
+        setScoreText();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveX, moveY);
+        rb2D.AddForce(push * movement);
+	}
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(name + "出发了" + other.name);
+        if (other.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score += 1;
+            setScoreText();
+        }
+    }
+
+    void setScoreText()
+    {
+        countText.text = "分数：" + score.ToString();
+        if (score >= 12)
+        {
+            winText.text = "GAME OVER";
+        }
+    }
+}
